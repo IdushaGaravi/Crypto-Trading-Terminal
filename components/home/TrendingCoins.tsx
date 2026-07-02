3570942429fb5bccc3fce13dbe4bcb61906c59ec
IdushaGaravi <idushagaravi@gmail.com>
2026-07-03 02:22:38 +0530

    fix some critical issues

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -4,9 +4,16 @@ import { TrendingDown, TrendingUp } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image'
 import DataTable from '../DataTable';
+import { TrendingCoinsFallback } from './fallback';
 
 const TrendingCoins = async () => {
-    const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300);
+    let trendingCoins;
+    try {
+        trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300); 
+    } catch (error) {
+        console.error('Error fetching trending coins:', error);
+        return <TrendingCoinsFallback />; 
+    }
 
     const columns: DataTableColumn<TrendingCoin>[] = [
 
@@ -62,7 +69,6 @@ const TrendingCoins = async () => {
         <div id='trending-coins'>
             <h4>Trending Coins</h4>
 
-            <div id='trending-coins'>
                 <DataTable
                     data={trendingCoins.coins.slice(0, 6) || []} 
                     columns={columns} 
@@ -71,7 +77,6 @@ const TrendingCoins = async () => {
                     headerCellClassName='py-3!'
                     bodyCellClassName='py-2!'
                 />
-            </div>
         </div>
 
     )
