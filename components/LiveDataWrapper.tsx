f0327af78c2a0870567f50cf4ad06d305f2c2917
IdushaGaravi <idushagaravi@gmail.com>
2026-07-08 10:06:04 +0530

    implement real time coin chart

--- components/LiveDataWrapper.tsx
+++ components/LiveDataWrapper.tsx
@@ -5,9 +5,11 @@ import CandlestickChart from "./CandlestickChart"
 import { Separator } from "./ui/separator"
 import DataTable from "./DataTable";
 import { formatCurrency, timeAgo } from "@/lib/utils";
+import { useState } from "react";
 
 const LiveDataWrapper = ({ children, coinId, poolId, coin, coinOHLCData }: LiveDataProps) => {
-    const { trades } = useCoinGeckoWebSocket({ coinId, poolId });
+    const [liveInterval, setLiveInterval] = useState<'1s' | '1m'>('1s');
+    const { trades, ohlcv } = useCoinGeckoWebSocket({ coinId, poolId, liveInterval });
 
     const tradeColumns: DataTableColumn<Trade>[] = [
     {
@@ -47,7 +49,15 @@ const LiveDataWrapper = ({ children, coinId, poolId, coin, coinOHLCData }: LiveD
             <p>Coin Header</p>
             <Separator className="divider" />
             <div className="trend">
-                <CandlestickChart coinId={coinId} data={coinOHLCData}>
+                <CandlestickChart 
+                    coinId={coinId} 
+                    data={coinOHLCData}
+                    liveOhlcv={ohlcv}
+                    mode="live"
+                    initialPeriod="daily"
+                    liveInterval={liveInterval}
+                    setLiveInterval={setLiveInterval}
+                >
                     <h4>Trend Overview</h4>
                 </CandlestickChart>
             </div>
